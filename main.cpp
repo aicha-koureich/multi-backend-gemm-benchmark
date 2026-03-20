9d7a17279a0826a550ec8674b1b24b9e85e1d64c
Aïcha KOUREICH <aicha.koureich@telecom-sudparis.eu>
2026-03-20 21:20:48 +0100

    Rearranged main's logic

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 #include "gemm_cpu.hpp"
 #include "cl_backend.hpp"
@@ -25,23 +27,22 @@ int main(){
     time_point<steady_clock> start, end;
     duration<double> duration_cpu, duration_cl;
     start = steady_clock::now();
-    GEMM_CPU(A,B,C,N);
-    end = steady_clock::now();
-    duration_cpu = end - start;
-    start= steady_clock::now();
     GEMM_OPENCL(A,B,C,N);
-    end= steady_clock::now();
-    duration_cl = end-start;
-
-    double GFLOPS0 = (2.0*N*N*N)/(duration_cpu.count()*1e9);
+    end = steady_clock::now();
+    duration_cl = end - start;
     double GFLOPS1 = (2.0*N*N*N)/(duration_cl.count()*1e9);
     cout<< "-----------RESULTS----------\n";
     cout<< "\n";
-    cout<< "CPU Compute time: " << duration_cpu.count()<<" s\n";
-    cout<<"CPU GFLOPS: "<<GFLOPS0<<" gflops/s\n";
-    cout<< "\n";
     cout<< "GPU w/opencl Compute time: " << duration_cl.count()<<" s\n";
     cout<<"GPU w/opencl GFLOPS: "<<GFLOPS1<<" gflops/s\n";
+    start= steady_clock::now();
+    GEMM_CPU(A,B,C,N);
+    end= steady_clock::now();
+    duration_cpu = end-start;
+    double GFLOPS0 = (2.0*N*N*N)/(duration_cpu.count()*1e9);
+    cout<< "\n";
+    cout<< "CPU Compute time: " << duration_cpu.count()<<" s\n";
+    cout<<"CPU GFLOPS: "<<GFLOPS0<<" gflops/s\n";
     delete[] A; 
     delete[] B; 
     delete[] C;
