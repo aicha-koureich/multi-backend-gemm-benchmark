d9269e84d0dd93d3228798ce94b7207712f20793
Aïcha Koureich <koureichaicha@gmail.com>
2026-04-27 13:23:50 +0200

    CUDAvsOPENCL

--- results.py
+++ results.py
@@ -6,7 +6,8 @@ N = [64, 128, 256, 512, 1024, 2048, 4096, 8192]
 
 def benchmark(x):
     res_cpu = [] 
-    res_gpu = [] 
+    res_cuda = [] 
+    res_opencl = [] 
     for n in N:
         print(f"Running N= {n}")
         cmd = subprocess.check_output([f"./benchmark", str(n), str(x)])
@@ -19,12 +20,12 @@ def benchmark(x):
             if x==0:
                 if "GPU w/cuda GFLOPS:" in line:
                     value = line.split(":")[1].split()[0]
-                    res_gpu.append(float(value))
+                    res_cuda.append(float(value))
             if x==1:
                 if "GPU w/opencl GFLOPS:" in line:
                     value = line.split(":")[1].split()[0]
-                    res_gpu.append(float(value))
-    return res_gpu, res_cpu
+                    res_opencl.append(float(value))
+    return res_cuda, res_opencl, res_cpu
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -33,28 +34,29 @@ if __name__ == "__main__":
     selected_mode = int(sys.argv[1])
     
     # Run the benchmark with that mode
-    # We ignore the other list returns by using "_"
     if selected_mode == 0:
-        print("Running CUDA Benchmark")
-        res_gpu, res_cpu = benchmark(0)
-        label = "CUDA"
+        print("Running CUDA vs CPU Benchmark")
+        res_cuda, _, res_cpu = benchmark(0)
     elif selected_mode == 1:
-        print("Running OpenCL Benchmark")
-        res_gpu, res_cpu = benchmark(1)
-        label = "OpenCL"
+        print("Running OpenCL vs CPU Benchmark")
+        _, res_opencl, res_cpu = benchmark(1)
+    elif selected_mode == 2:
+        print("Running CUDA vs OpenCL Benchmark")
+        res_cuda, _, res_cpu = benchmark(0)
+        _, res_opencl, _ = benchmark(1)
     else:
-        print("Error: Invalid mode. Use 0 for CUDA or 1 for OpenCL.")
+        print("Error: Invalid mode. Use 0 for CUDA, 1 for OpenCL, 2 for both.")
         sys.exit(1)
 
     fig, ax1 = plt.subplots(1, figsize=(10, 6))
-    if label == "CUDA":
-        fig.suptitle('GEMM Benchmark: Cuda (GTX 1660 Super) vs CPU (Ryzen 5 3600)', fontsize=10)
-        ax1.plot(N, res_gpu,  marker='o', label='CUDA' )
-    elif label == "OpenCL":
-        fig.suptitle('GEMM Benchmark: OpenCL (Intel UHD Graphics) vs CPU (Intel)', fontsize=10)
-        ax1.plot(N, res_gpu,  marker='o', label='OpenCL' )
-
-    ax1.plot(N, res_cpu, marker='o', label='CPU')
+    if res_cuda:
+        ax1.plot(N, res_cuda, marker='s', label='GPU CUDA', color='green')
+    if res_opencl:
+        ax1.plot(N, res_opencl, marker='^', label='GPU OpenCL', color='orange')
+    if res_cpu:
+        ax1.plot(N, res_cpu, marker='o', label='CPU', color='blue')
+    title = "Cuda vs OpenCL (Tesla T4)" if selected_mode == 2 else f" { 'CUDA vs CPU (Tesla T4)' if selected_mode == 0 else 'OpenCL vs CPU (Tesla T4)' }"
+    fig.suptitle(f'GEMM Benchmark: {title}', fontsize=12)
 
     ax1.set_xlabel('Matrix size N')
     ax1.set_ylabel('GFLOPS')
